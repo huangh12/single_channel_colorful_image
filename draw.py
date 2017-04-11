26706b365ca84589379f17d9dd550477ff0063e3
huangh12 <huanghe2016@ia.ac.cn>
2017-04-11 19:22:42 +0800

    first upload

---
+++ draw.py
@@ -0,0 +1,33 @@
+'''
+ reference: http://stackoverflow.com/questions/3114925/pil-convert-rgb-image-to-a-specific-8-bit-palette
+            http://abruzzi.iteye.com/blog/314790
+			http://www.2cto.com/kf/201603/492898.html
+
+'''
+from PIL import Image
+import numpy as np
+from datasets import CONFIG
+
+# The im is a predicted result of a cat
+arr = np.load('arr.npy')
+
+print 'The shape of the image is:', arr.shape
+print 'The classes in the image are:', np.unique(arr)
+
+# Define the palette
+palette = []
+for i in range(256):
+	palette.extend((i, i, i))
+	
+# define the color of the 21 classes(PASACAL VOC)
+palette[:3*21] = CONFIG['voc12']['palette'].flatten()
+
+assert len(palette) == 768
+
+im = Image.fromarray(arr)
+im.show()
+im.putpalette(palette)
+im.show()
+
+im.save('out.png')
+
